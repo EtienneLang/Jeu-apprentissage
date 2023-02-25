5673c22590a0f0a3e199686fddd2e26de2140209
WillMatte <williamatte@gmail.com>
2023-02-25 13:17:54 -0500

    Ajouts des pages du menu
    Tous les pages du menu sont fonctionnel, manque seulement à ce que les settings changent quand on les sélectionnes

--- Lost in Dystopia/Assets/Scripts/MenuPrincipal.cs
+++ Lost in Dystopia/Assets/Scripts/MenuPrincipal.cs
@@ -6,9 +6,20 @@ using UnityEngine.SceneManagement;
 public class MenuPrincipal : MonoBehaviour
 {
 
+    /// <summary>
+    /// Permets de commencer le jeu
+    /// </summary>
     public void DemarrerJeu() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void FermerJeu() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //Debug.Log("Quitter le jeu");
+        //Application.Quit();
     }
 
 
