3d4fe1696ace24ac8940c13218d96f8554af22d4
WillMatte <williamatte@gmail.com>
2023-02-25 15:47:13 -0500

    Ajout du menu de pause

---
+++ Lost in Dystopia/Assets/Scripts/MenuPause.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+
+    public static bool JeuEnPause = false;
+    public GameObject MenuPauseUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (JeuEnPause)
+            {
+                ContinuerJeu();
+            }
+            else
+            {
+                MettreJeuEnPause();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Permets de continuer le jeu
+    /// </summary>
+    public void ContinuerJeu() 
+    {
+        MenuPauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        JeuEnPause = false;
+    }
+
+    /// <summary>
+    /// Permets de mettre le jeu en pause
+    /// </summary>
+    public void MettreJeuEnPause()
+    {
+        MenuPauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        JeuEnPause = true;
+    }
+
+    /// <summary>
+    /// Permets de quitter le jeu
+    /// </summary>
+    public void QuitterJeu() 
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Permets de retourner au menu principal
+    /// </summary>
+    public void RetournerMenuPrincipal() 
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+}
