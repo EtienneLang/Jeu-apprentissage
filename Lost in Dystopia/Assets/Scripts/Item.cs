72e6745e1c327f79e54dd3aff373c1fcf01c74b7
WillMatte <williamatte@gmail.com>
2023-02-25 23:28:19 -0500

    Début de l'inventaire

---
+++ Lost in Dystopia/Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Nouvel item", menuName ="Iventory/Item")]
+public class Item : ScriptableObject
+{
+
+    public string Nom;
+    public Sprite Icon;
+
+
+
+}
