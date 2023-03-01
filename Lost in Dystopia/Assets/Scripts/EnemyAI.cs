01bb7e88763a239726674ca1719c5ecf40ec18a4
langloise <etiennelanglois007@gmail.com>
2023-03-01 10:00:28 -0500

    Ajout de A* pathfinder, ennemi suit le joueur
    Pas encore d'annimation pour l'ennemi

---
+++ Lost in Dystopia/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float speed = 200f;
+    public float nextWaypointdistance = 3f;
+    Path path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
