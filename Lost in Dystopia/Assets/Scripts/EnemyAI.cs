6ffa9aa413bb7bc0ecd951347a4cf42b8434fa8e
langloise <etiennelanglois007@gmail.com>
2023-04-06 15:51:44 -0400

    LostOne Patrouille terminé

--- Lost in Dystopia/Assets/Scripts/EnemyAI.cs
+++ Lost in Dystopia/Assets/Scripts/EnemyAI.cs
@@ -20,11 +20,13 @@ public class EnemyAI : MonoBehaviour
     private Rigidbody2D _rb;
     private GameObject player;
     public Transform enemyGFX;
+    public Patrol patrol;
     public const float DISTANCE_REPERAGE = 4f;
 
     // Start is called before the first frame update
     void Start()
     {
+        patrol = GetComponent<Patrol>();
         player = GameObject.FindGameObjectWithTag("Player");
         _seeker = GetComponent<Seeker>();
         _rb = GetComponent<Rigidbody2D>();
@@ -45,31 +47,19 @@ public class EnemyAI : MonoBehaviour
         }
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
-        if (_path == null)
-        {
-            return;
-        }
-
-        if (_currentWaypoint >= _path.vectorPath.Count)
-        {
-            _reachedEndOfPath = true;
-            return;
-        }
-        else
-        {
-            _reachedEndOfPath = false;
-        }
-
         float distance = Vector3.Distance(player.transform.position, _rb.transform.position);
-        if (distance < 4f)
+        if (distance < 2f)
         {
             _target = player.transform;
-            Debug.Log("Le joueur est assez proche");
-            InvokeRepeating("UpdatePath", 0f, .2f);
             _targetIsSeen = true;
+            patrol.delay = 0;
+            for (int i = 0; i < patrol.targets.Length; i++)
+            {
+                patrol.targets[i] = player.transform;
+            }
+            
         }
         if (_targetIsSeen)
         {
