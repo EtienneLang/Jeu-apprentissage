35730c82791227ff02f90e30f2b8f4f2d6378872
langloise <etiennelanglois007@gmail.com>
2023-04-25 13:24:54 -0400

    Curseur fleche dans menu principal et pause

--- Lost in Dystopia/Assets/Scripts/CursorManager.cs
+++ Lost in Dystopia/Assets/Scripts/CursorManager.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class CursorManager : MonoBehaviour
 {
     [SerializeField] private Texture2D curseurBase;
     [SerializeField] private Texture2D curseurMain;
     [SerializeField] private Texture2D curseurMainFerme;
+    [SerializeField] private Texture2D curseurFleche;
+    private GameObject menuPause;
+
     private bool baseActive = true;
     private Vector2 cursorHotspot;
     // Start is called before the first frame update
     void Start()
     {
+        menuPause = GameObject.FindGameObjectWithTag("MenuPause");
         cursorHotspot = new Vector2(curseurBase.width / 2, curseurBase.height / 2);
         Cursor.SetCursor(curseurBase, cursorHotspot, CursorMode.Auto);
     }
@@ -22,8 +28,15 @@ public class CursorManager : MonoBehaviour
     {
         if (EventSystem.current.IsPointerOverGameObject()) 
         {
+            menuPause = GameObject.FindGameObjectWithTag("MenuPause");
             baseActive = false;
-            if (Input.GetMouseButton(0))
+            if (SceneManager.GetActiveScene().name == "MenuPrincipal" || menuPause != null)
+            {
+                cursorHotspot = new Vector2(curseurFleche.width / 2, curseurFleche.height / 2);
+                Cursor.SetCursor(curseurFleche, cursorHotspot, CursorMode.Auto);
+            }
+            
+            else if (Input.GetMouseButton(0))
             {
                 cursorHotspot = new Vector2(curseurMainFerme.width / 2, curseurMainFerme.height / 2);
                 Cursor.SetCursor(curseurMainFerme, cursorHotspot, CursorMode.Auto);
